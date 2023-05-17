f309b851cd54c3a308b1404caaf2cca8001b9fdc
Brendan Higginbottom <brendan.higginbottom@gmail.com>
2023-05-17 17:21:09 -0500

    moved delay line json into public folder

--- src/components/MainView/MainView.jsx
+++ src/components/MainView/MainView.jsx
@@ -1,12 +1,30 @@
-
+// Get createDevice from rnbo.js library
+const { createDevice } = require('@rnbo/js');
 
 
 function MainView() {
+
+    //Creating device (delay line) from the exported JSON file
+    const setup = async() => {
+        const patchExportURL = "export/rnbo.filerdelay.json";
+        console.log(patchExportURL);
+        let rawPatcher = await fetch(patchExportURL);
+        let patcher = await rawPatcher.json();
+
+        let device = await createDevice({ context, patcher });
+
+        //Not sure what destination will end up being, noticing RNBO plays thru comp speakers even if I have
+        //headphones connected
+        device.node.connect(context.destination);
+    };
+
+    setup();
 
     //Creating WebAudio AudioContext
     let WAContext = window.AudioContext || window.webkitAudioContext;
     let context = new WAContext();
 
+    //Resuming audiocontext with user gesture (the onClick)
     const resumeAudioContext = () => {
         context.resume();
         console.log('resuming audio context');
