1771427435dfc5f173b401f084a3905864b5b5f5
Brendan Higginbottom <brendan.higginbottom@gmail.com>
2023-05-16 20:28:59 -0500

    added WebAudio AudioContext and resume context function tied to a button in MainView component

--- src/components/MainView/MainView.jsx
+++ src/components/MainView/MainView.jsx
@@ -3,9 +3,20 @@
 
 function MainView() {
 
+    //Creating WebAudio AudioContext
+    let WAContext = window.AudioContext || window.webkitAudioContext;
+    let context = new WAContext();
+
+    const resumeAudioContext = () => {
+        context.resume();
+        console.log('resuming audio context');
+    }
+
+
     return (
         <>
             <h1>Main View!</h1>
+            <button onClick={resumeAudioContext}>Test Button</button>
         </>
     );
 }
