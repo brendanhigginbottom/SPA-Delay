00151247a9f8fc2b40619998a05fdb9598dc085b
Brendan Higginbottom <brendan.higginbottom@gmail.com>
2023-05-17 19:01:34 -0500

    device setup now working (accessing local json file)

--- src/components/MainView/MainView.jsx
+++ src/components/MainView/MainView.jsx
@@ -1,24 +1,37 @@
 // Get createDevice from rnbo.js library
 const { createDevice } = require('@rnbo/js');
+// Storing init delay in state
+import { useState, useEffect } from 'react';
 
 
 function MainView() {
+    const [delay, setDelay] = useState({});
 
     //Creating device (delay line) from the exported JSON file
     const setup = async() => {
-        const patchExportURL = "export/rnbo.filerdelay.json";
+        const patchExportURL = "/export/rnbo.filterdelay.json";
         console.log(patchExportURL);
-        let rawPatcher = await fetch(patchExportURL);
+        let rawPatcher = await fetch(patchExportURL,{
+            headers : { 
+              'Content-Type': 'application/json',
+              'Accept': 'application/json'
+             }
+          }
+          );
         let patcher = await rawPatcher.json();
 
         let device = await createDevice({ context, patcher });
+        console.log(device);
 
         //Not sure what destination will end up being, noticing RNBO plays thru comp speakers even if I have
         //headphones connected
         device.node.connect(context.destination);
     };
 
-    setup();
+    useEffect(() => {
+        setup();
+      }, []);
+    
 
     //Creating WebAudio AudioContext
     let WAContext = window.AudioContext || window.webkitAudioContext;
