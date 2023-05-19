0ac7f338df49747486cace7f7faa14b62ab25a11
Brendan Higginbottom <brendan.higginbottom@gmail.com>
2023-05-18 21:08:33 -0500

    cleanup, pushing to post on issue board

--- src/components/WebAudioTest/WebAudioTest.jsx
+++ src/components/WebAudioTest/WebAudioTest.jsx
@@ -2,14 +2,16 @@ import { useState, useEffect } from 'react';
 
 function WebAudioTest() {
 
+     // Create instance of audio context
+     const AudioContext = window.AudioContext || window.webkitAudioContext;
+     const audioContext = new AudioContext();
+
     const setup = () => {
-         // Create instance of audio context
-    const AudioContext = window.AudioContext || window.webkitAudioContext;
-    const audioContext = new AudioContext();
 
     // get the audio element
     const audioElement = document.querySelector("audio")
     console.log(document.querySelector("audio"));
+    // create new MediaElementAudioSourceNode
     const track = audioContext.createMediaElementSource(audioElement);
     };
 
@@ -18,15 +20,10 @@ function WebAudioTest() {
     }, [])
 
    
-
-
-   
-    
-
     return (
         <>
             <h1>Test 123 ABC</h1>
-            <audio id="audio" src="/export/media/SPADelayTest.mp3" />
+            <audio id="audio" src="./export/media/SPADelayTest.mp3" />
         </>
     );
 }
