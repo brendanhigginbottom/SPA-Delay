69ee9bb31da43d3183fedf4422b05ad35f3a2f97
Brendan Higginbottom <brendan.higginbottom@gmail.com>
2023-06-05 19:01:59 -0500

    added to about page

--- src/components/AboutPage/AboutPage.jsx
+++ src/components/AboutPage/AboutPage.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import './AboutPage.css';
+
 // This is one of our simplest components
 // It doesn't have local state,
 // It doesn't dispatch any redux actions or display any part of redux state
@@ -9,7 +11,24 @@ function AboutPage() {
   return (
     <div className="container">
       <div>
-        <p>SPA-Delay is a Single Page Application (SPA) and a Signal-Processing Application (SPA). It uses React, Axios, PG, and MUI alongside the WebAudio API and the Filter Delay from RNBO by Cycling '74 for signal processing.</p>
+        <p>
+          SPA-Delay is a Single Page Application (SPA) and a Signal-Processing Application (SPA). 
+          It uses React, Axios, PG, and MUI alongside the&nbsp;
+           <a target='_blank' href='https://developer.mozilla.org/en-US/docs/Web/API/Web_Audio_API'>WebAudio API</a> 
+           &nbsp;and the Filter Delay from&nbsp;
+           <a target='_blank' href='https://rnbo.cycling74.com/'>RNBO</a> 
+           &nbsp;by Cycling '74 for signal processing.
+        </p> <br />
+        <p>All sound files are my own except for "Ham Radio Operators" which features two unknown
+          HAM radio operators recorded using the&nbsp; 
+          <a target='_blank' href='http://websdr.ewi.utwente.nl:8901/'> Wide-band WebSDR</a>
+          &nbsp;by the University of Twente.
+        </p> <br />
+        <p>SPA-Delay was created as my Solo Project for Prime Digital Academy. 
+          Thank you to my cohort, the instructors at Prime, my family, and my partner. 
+          None of this would have happened without you.
+        </p>
+
       </div>
     </div>
   );
